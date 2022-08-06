40670a6475fbddb198e801005c81bef1442239f2
Peter Müller <peter7995@gmail.com>
2022-08-06 14:46:16 +0200

    add generic example

--- examples/string.go
+++ examples/string.go
@@ -3,11 +3,20 @@ package examples
 import (
 	"fmt"
 	"strings"
+
+	"github.com/peter-mueller/rel"
 )
 
 func Insert(p Person) (stmt string, params []any) {
 	stmt += fmt.Sprintf("INSERT INTO person (%s)\n", strings.Join(person.AttributeRef(), ", "))
 	stmt += fmt.Sprintf("VALUES (%s)\n", strings.Join(person.Params("?"), ","))
 	params = person.Tuple(p)
+	return stmt, params
+}
+
+func InsertGeneric[T any](v *rel.Var[T], relName string, value T) (stmt string, params []any) {
+	stmt += fmt.Sprintf("INSERT INTO %s (%s)\n", relName, strings.Join(v.AttributeRef(), ", "))
+	stmt += fmt.Sprintf("VALUES (%s)\n", strings.Join(v.Params("?"), ","))
+	params = v.Tuple(value)
 	return stmt, params
 }
--- examples/string_test.go
+++ examples/string_test.go
@@ -1,10 +1,30 @@
 package examples
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/peter-mueller/rel"
+)
 
 func TestStringInsert(t *testing.T) {
 	p := Person{Name: "Hans", Age: 33}
 	stmt, params := Insert(p)
+	expected := "INSERT INTO person (name, age)\nVALUES (?,?)\n"
+	if stmt != expected {
+		t.Fatalf("stmt was\n\n%s\nbut should be\n\n%s", stmt, expected)
+	}
+	expectedParams := []any{"Hans", 33}
+	for i := range expectedParams {
+		if expectedParams[i] != params[i] {
+			t.Fatalf("params were %v but should be%v", params, expectedParams)
+		}
+	}
+}
+
+func TestStringInsertGeneric(t *testing.T) {
+	p := Person{Name: "Hans", Age: 33}
+	v := rel.NewVar[Person]()
+	stmt, params := InsertGeneric(v, "person", p)
 	expected := "INSERT INTO person (name, age)\nVALUES (?,?)\n"
 	if stmt != expected {
 		t.Fatalf("stmt was\n\n%s\nbut should be\n\n%s", stmt, expected)
